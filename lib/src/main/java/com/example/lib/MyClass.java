2dff353de2d8b47f6fbf4939d2c59918b661a712
cc019950905 <cc033212000@gmail.com>
2019-11-01 15:01:47 +0800

    Initial commit

---
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -0,0 +1,15 @@
+package com.example.lib;
+
+public class MyClass {
+    public static void main(String[]arg){
+        int i,j;
+        for(i=1;i<10;i++){
+            for(j=1;j<10;j++){
+                System.out.print(i+" * " + j +" = " +(i*j)+" ");
+
+            }
+            System.out.println();
+        }
+
+    }
+}
